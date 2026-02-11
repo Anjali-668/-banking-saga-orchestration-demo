973f53c423f6aa2c5f86e3dacc9e48a767043c06
Anjali-668 <Anjali.sreekumar@ust.com>
2026-02-11 14:24:45 +0530

    Update start_workflow.sh

--- scripts/start_workflow.sh
+++ scripts/start_workflow.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 CONDUCTOR=${CONDUCTOR:-http://localhost:8080/api}
 INPUT=${1:-'{"applicant":"ANJALI","amount":80000}'}
 
-echo "Starting loan_saga_workflow with input: $INPUT"
-WF_ID=$(curl -sS -X POST -H "Content-Type: application/json"   -d "$INPUT"   "$CONDUCTOR/workflow/loan_saga_workflow")
+echo "Starting saga_workflow with input: $INPUT"
+WF_ID=$(curl -sS -X POST -H "Content-Type: application/json"   -d "$INPUT"   "$CONDUCTOR/workflow/saga_workflow")
 
 echo "Workflow ID: $WF_ID"
 
